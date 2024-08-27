8fc25ea02faa1278b847b271899f3b8cf646f0d8
ThoHoang0505 <thomagic1@gmail.com>
2024-08-27 16:48:54 +0700

    change database

--- toasoan.sql
+++ toasoan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 15, 2024 lúc 10:42 AM
+-- Thời gian đã tạo: Th8 26, 2024 lúc 03:44 PM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -49,7 +49,43 @@ INSERT INTO `bantinhientruong` (`MaBanTinHienTruong`, `LoaiTin`, `TieuDe`, `NoiD
 (45, 'LT08', 'TEST FINNAL', '<b>ABCD</b>\r\n<span style=\'color:blue\'><u><i><b>ABCD</b></i></u></span>', 'uploads/images/CoopTeam_3.png', 'uploads/videos/3195394-uhd_3840_2160_25fps.mp4', '2024-04-15', 'FINNAL ABC'),
 (46, 'LT01', 'TEST FINNAL 2', '123\r\n<u><i><b>ABCDE</b></i></u>', 'uploads/images/Moon.jpg', 'uploads/videos/3195394-uhd_3840_2160_25fps.mp4', '2024-04-15', 'FINNAL 2'),
 (47, 'LT06', 'VĂN HỌC NGA 1965', '<i><b>Tiêu Đề In Đậm Và In Nghiêng</b></i>\r\n<i>Nội Dung 1</i>\r\n<u>Nội Dung 2</u>\r\n<span style=\'color:blue\'>Nội Dung 3</span>', 'uploads/images/CoopTeam_3.png', 'uploads/videos/3195394-uhd_3840_2160_25fps.mp4', '2024-04-15', 'BÁO NGA 2024'),
-(48, 'LT04', 'DỊCH COVIT CÓ KHẢ NĂNG BÙNG PHADT TRỞ LẠI TẠI TRUNG QUỐC NĂM 2024', '<b>ABCD</b>\r\n<i>ABCDE</i>', 'uploads/images/CoopTeam_1.png', 'uploads/videos/3195394-uhd_3840_2160_25fps.mp4', '2024-04-15', 'TRUNG QUỐC');
+(48, 'LT04', 'DỊCH COVIT CÓ KHẢ NĂNG BÙNG PHADT TRỞ LẠI TẠI TRUNG QUỐC NĂM 2024', '<b>ABCD</b>\r\n<i>ABCDE</i>', 'uploads/images/CoopTeam_1.png', 'uploads/videos/3195394-uhd_3840_2160_25fps.mp4', '2024-04-15', 'TRUNG QUỐC'),
+(49, 'LT11', 'CÔNG VĂN CHÍNH PHỦ 1', '<b>ABCD</b>\r\n1234', 'uploads/images/CoopTeam_3.png', 'uploads/videos/3195394-uhd_3840_2160_25fps.mp4', '2024-04-15', 'VIỆT NAM'),
+(50, 'LT01', 'TEST 2 ẢNH', 'abcd', 'uploads/images/Moon.jpg', 'uploads/videos/3195394-uhd_3840_2160_25fps.mp4', '2024-04-15', 'abc'),
+(51, 'LTX9', 'Thoi Tiet Ha Noi 2024', '<b>Thoi Tiet Ha Noi 2024</b>\r\n<i>Ha Noi mua nhieu noi</i>', 'uploads/images/Moon.jpg', 'uploads/videos/3195394-uhd_3840_2160_25fps.mp4', '2024-08-26', 'Ha Noi');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `docgia`
+--
+
+CREATE TABLE `docgia` (
+  `MaDG` int(11) NOT NULL,
+  `TenDG` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `NgaySinh` date NOT NULL,
+  `SDT` varchar(12) NOT NULL,
+  `DiaChi` varchar(50) NOT NULL,
+  `GioiTinh` varchar(10) NOT NULL,
+  `MaTK_DG` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `docgia`
+--
+
+INSERT INTO `docgia` (`MaDG`, `TenDG`, `Email`, `NgaySinh`, `SDT`, `DiaChi`, `GioiTinh`, `MaTK_DG`) VALUES
+(1, 'Doc Gia TEST 3', 'example@st.vimaru.edu.vn', '2005-05-05', '0123456789', 'Ha Noi', 'male', 1),
+(2, 'Doc Gia TEST 2', 'tho88816@st.vimaru.edu.vn', '2003-05-05', '01245567', 'Ha Noi', 'male', 2),
+(3, 'Doc Gia 3', 'thomusedash@gmail.com', '2002-05-05', '0123344556', 'Hai Phong', 'male', 3),
+(4, 'Tho Hoang', 'tho88816@st.vimaru.edu.vn', '2002-05-05', '123456', 'Hai Phong', 'Nam', 4),
+(5, 'Tran Hoang Tho', 'tho@st.vimaru.edu.vn', '2002-05-05', '12345678', 'Hai Phong', 'Nữ', 5),
+(6, 'Tran Hoang Tho', 'tho88816@st.vimaru.edu.vn', '2002-05-05', '1233123123', 'Hai Phong', 'Nam', 6),
+(7, 'Hello World', 'tho123@edu.vn', '2003-01-01', '124568998', 'Hai Phong', 'Nam', 7),
+(8, 'THT', 'tho@st.vimaru.edu.vn', '2002-05-05', '123123123', 'Ha Noi', 'Nữ', 8),
+(9, 'thttest2', 'thomagic1@gmail.com', '2002-01-01', '123123123', 'Ha Noi', 'Nữ', 9),
+(10, 'THT2', 'trinhthinhnormie@gmail.com', '2002-05-05', '123123123123', 'Ha Noi', 'Nam', 10);
 
 -- --------------------------------------------------------
 
@@ -67,14 +103,44 @@ CREATE TABLE `loaitin` (
 --
 
 INSERT INTO `loaitin` (`MaLT`, `TenLT`) VALUES
-('LT01', 'Trong Nước'),
 ('LT02', 'Chính Trị'),
 ('LT03', 'Quân Sự'),
 ('LT04', 'Quốc Tế'),
 ('LT05', 'Y Tế'),
 ('LT06', 'Văn Học'),
 ('LT07', 'Test 1'),
-('LT08', 'Test 2');
+('LT08', 'Test 2'),
+('LT11', 'Công Văn Chính Phủ'),
+('LTX9', 'Tin Cảnh Báo Thời Tiết');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `taikhoan`
+--
+
+CREATE TABLE `taikhoan` (
+  `MaTK` int(11) NOT NULL,
+  `TenDangNhap` varchar(30) NOT NULL,
+  `MatKhau` varchar(30) NOT NULL,
+  `Quyen` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `taikhoan`
+--
+
+INSERT INTO `taikhoan` (`MaTK`, `TenDangNhap`, `MatKhau`, `Quyen`) VALUES
+(1, 'Mmmmm0709', '123', 6),
+(2, '88816', '123', 6),
+(3, 'Mmmmm0505', '123', 6),
+(4, 'thotest', '123', 6),
+(5, 'thotest2', '1234', 6),
+(6, 'thotest3', '1234', 6),
+(7, 'helloworld', '1234', 6),
+(8, 'thttest', '123', 6),
+(9, 'thttest2', '123', 6),
+(10, 'thttest3', '123', 6);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -87,10 +153,23 @@ ALTER TABLE `bantinhientruong`
   ADD KEY `TuTang` (`MaBanTinHienTruong`);
 
 --
+-- Chỉ mục cho bảng `docgia`
+--
+ALTER TABLE `docgia`
+  ADD KEY `TuTang` (`MaDG`),
+  ADD KEY `MaTK_DG` (`MaTK_DG`);
+
+--
 -- Chỉ mục cho bảng `loaitin`
 --
 ALTER TABLE `loaitin`
   ADD PRIMARY KEY (`MaLT`);
+
+--
+-- Chỉ mục cho bảng `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD KEY `TuTang` (`MaTK`);
 
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
@@ -100,7 +179,29 @@ ALTER TABLE `loaitin`
 -- AUTO_INCREMENT cho bảng `bantinhientruong`
 --
 ALTER TABLE `bantinhientruong`
-  MODIFY `MaBanTinHienTruong` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `MaBanTinHienTruong` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- AUTO_INCREMENT cho bảng `docgia`
+--
+ALTER TABLE `docgia`
+  MODIFY `MaDG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT cho bảng `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  MODIFY `MaTK` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `docgia`
+--
+ALTER TABLE `docgia`
+  ADD CONSTRAINT `docgia_ibfk_1` FOREIGN KEY (`MaTK_DG`) REFERENCES `taikhoan` (`MaTK`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
